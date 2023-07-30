8225d5d322e4bfa519c2200e131e315b0da23f8e
filipton <filipton12@gmail.com>
2023-07-30 22:07:44 +0200

    feat: cli "installer" (#skip)

--- cli.sh
+++ cli.sh
@@ -1,8 +1,7 @@
-devc() { 
-    if [ -z "$1" ]; then
-        echo "Usage: devc <enviroment name>"
-        return 1
-    fi
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: devc <enviroment name>"
+    exit 1
+fi
 
-    docker run --net host --hostname "$1-devc" -v ./:/app --rm -it filipton/"$1"-devc:latest bash 
-}
+docker run --net host --hostname "$1-devc" -v ./:/app --rm -it filipton/"$1"-devc:latest bash 
---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo cp ./cli.sh /bin/devc
