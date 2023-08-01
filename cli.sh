73984261272f7cfd445e508b95ff793be76c8225
filipton <filipton12@gmail.com>
2023-08-01 21:35:25 +0200

    feat: devc command docker run opts (#skip)

--- cli.sh
+++ cli.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    echo "Usage: devc <enviroment name>"
+    echo "Usage: devc <enviroment name> <optional: docker run opts>"
     exit 1
 fi
 
-docker run --net host --hostname "$1-devc" -v ./:/app --rm -it filipton/"$1"-devc:latest bash 
+if [ "$1" = "list" ]; then
+    echo "Available enviroments:"
+    docker search --limit 100 "filipton/" | grep devc | cut -d ' ' -f1 | cut -d '/' -f2 | sed -e 's/-devc//'
+    exit 1
+fi
+
+docker run $2 --net host --hostname "$1-devc" -v ./:/app --rm -it filipton/"$1"-devc:latest bash 
